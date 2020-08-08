9128a2fce8464e6971395f840bdf035fb9305a32
hahayoon <wuis749@naver.com>
2020-08-08 12:19:28 +0900

    프로젝트 초기화 init

---
+++ src/test/java/site/hahayoon/todoList/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package site.hahayoon.todoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
